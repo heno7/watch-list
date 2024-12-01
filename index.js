3cb3f7dd9c9ac870417b3bfe612b8e95e0ad960d
heno7 <105067953+heno7@users.noreply.github.com>
2024-12-01 21:14:18 +0700

    change api call to use https

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=3885fdf7&'
+const BASE_URL = 'https://www.omdbapi.com/?apikey=3885fdf7&'
 
 const searchBtn = document.querySelector(".search-btn")
 const searchForm = document.getElementById('search-form')
